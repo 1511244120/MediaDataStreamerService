1e588b016a72c5fe3a5758d17d3cb26291b27d4c
1511244120 <gao15248062793@gmail.com>
2025-02-08 17:47:05 -0500

    refactor kafka logic and add database layer

---
+++ kafka/producer/src/models/artists.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, Text
+from database.models.base import Base
+
+class Artists(Base):
+    __tablename__ = 'artists'
+    
+    artist_id = Column(Integer, primary_key=True, autoincrement=True)
+    artist_name = Column(Text, nullable=False, unique=True)
--- kafka/producer/src/producer.py
+++ kafka/producer/src/producer.py
@@ -4,7 +4,8 @@ import random
 from datetime import datetime
 from typing import List, Dict, Any
 from kafka import KafkaProducer
-from faker import Faker
+from fake_event_generator import FakeTrackEventGenerator
+
 class Producer:
     def __init__(self, kafka_servers: List[str], topic: str) -> None:
         self.kafka_servers = kafka_servers
@@ -13,26 +14,18 @@ class Producer:
             bootstrap_servers=self.kafka_servers,
             value_serializer=lambda v: json.dumps(v).encode('utf-8')
         )
-        self.fake = Faker()
         self.hashtags = ['#data', '#AI', '#BigData', '#Kafka', '#Python', '#project',
                          '#developer', '#technology']
-    def generate_social_media_data(self) -> Dict[str, Any]:
-        data: Dict[str, Any] = {
-            "user_id": random.randint(1, 1000),
-            "track_id": random.randint(1, 50000),
-            "timestamp": self.fake.date_time_this_month().isoformat(),
-            "event_type": random.choices(["streaming", "like"],
-                                         weights=[0.9, 0.1],
-                                         k=1)[0]
-        }
-        return data
+        self.fake_event_generator = FakeTrackEventGenerator()
+    
     def send_data(self, data: Dict[str, Any]) -> None:
         self.producer.send(self.topic, data)
         self.producer.flush()
+
     def run(self, interval: float = 1.0) -> None:
         try:
             while True:
-                data = self.generate_social_media_data()
+                data = self.fake_event_generator.generate_event()
                 print(f"Generated social media data: {data}")
                 self.send_data(data)
                 time.sleep(interval)
@@ -40,8 +33,3 @@ class Producer:
             print("Stopping data generator.")
         finally:
             self.producer.close()
-if __name__ == '__main__':
-    kafka_servers = ['kafka:9092']  # Kafka broker address
-    topic = 'social_media_data'  # Topic to send data to
-    data_generator = Producer(kafka_servers, topic)
-    data_generator.run(interval=1.0) # Generate data every second
